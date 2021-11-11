c016f7301bdddee87a45dd35e7e54a20b83de804
Benga <benga.olasebikan@gmail.com>
2021-11-11 12:51:20 +0000

    landing page setup with join form with functionality to push user to the chat page. Will add reducer next

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,9 +1,35 @@
-function LoginForm() {
+import PropTypes from 'prop-types'
+import { Card, CardBody, CardTitle, Form, FormGroup, Label, Input, Button } from 'reactstrap'
+
+function LoginForm({ handleChange, handleSubmit }) {
   return (
-    <div>
-      Hello Form
-    </div>
+    <Card className="mt-5 p-2 align-self-center w-50">
+      <CardTitle tag="h2" className="text-center">Welcome to the best Chat App</CardTitle>
+      <CardBody>
+        <Form onSubmit={handleSubmit} className="p-3">
+          <FormGroup>
+            <Label for="username">
+              Username
+            </Label>
+            <Input
+              id="username"
+              name="username"
+              placeholder="Please enter your username"
+              type="text"
+              required
+              onChange={handleChange}
+            />
+          </FormGroup>
+          <Button>Join Now</Button>
+        </Form>
+      </CardBody>
+    </Card>
   )
+}
+
+LoginForm.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
 }
 
 export default LoginForm
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,35 @@
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { toast, ToastContainer } from 'react-toastify'
 import LoginForm from '../../components/form'
+import 'react-toastify/dist/ReactToastify.css'
 
 
 function Home() {
+  const [userName, setUserName] = useState('')
+
+  const history = useHistory()
+
+
+  const handleChange = e => setUserName(e.target.value)
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    toast.success(`Welcome to the chat app ${userName}`)
+
+    setTimeout(() => history.push('/chat'), 3000)
+  }
+
+
   return (
     <Fragment>
-      <LoginForm />
+      <ToastContainer />
+      <LoginForm handleChange={handleChange} handleSubmit={handleSubmit} />
     </Fragment>
   )
 }
 
+
 export default Home
+
